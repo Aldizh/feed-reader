8a22148d6548109b05cb92b3852e4229cf6bb608
Aldi Zhupani <aldizhupani@Coparts-MacBook-Pro-106.local>
2016-07-05 23:48:08 -0500

    Refactor menu testing

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -15,54 +15,53 @@ $(function() {
 		* feeds definitions, the allFeeds variable in our application.
 		*/
 		describe("RSS Feeds", function() {
-				/* This is our first test - it tests to make sure that the
-				 * allFeeds variable has been defined and that it is not
-				 * empty. Experiment with this before you get started on
-				 * the rest of this project. What happens when you change
-				 * allFeeds in app.js to be an empty array and refresh the
-				 * page?
-				 */
-				it('are defined', function() {
-						expect(allFeeds).toBeDefined();
-						expect(allFeeds.length).not.toBe(0);
-				});
+			/* This is our first test - it tests to make sure that the
+			 * allFeeds variable has been defined and that it is not
+			 * empty. Experiment with this before you get started on
+			 * the rest of this project. What happens when you change
+			 * allFeeds in app.js to be an empty array and refresh the
+			 * page?
+			 */
+			it('are defined', function() {
+					expect(allFeeds).toBeDefined();
+					expect(allFeeds.length).not.toBe(0);
+			});
 
+			/* Loop through each feed in the allFeeds object and ensure
+			 * it has a URL defined and that the URL is not empty.
+			 */
+			it('all should have a URL', function() {
+					for (var i = 0; i < allFeeds.length; i++){
+						expect(allFeeds[i].url).toBeDefined();
+						expect(allFeeds[i].url.length).toBeGreaterThan(0);
+					}
+			});
 
-				/* Loop through each feed in the allFeeds object and ensure
-				 * it has a URL defined and that the URL is not empty.
-				 */
-				it('all should have a URL', function() {
-						for (var i = 0; i < allFeeds.length; i++){
-							expect(allFeeds[i].url).toBeDefined();
-							expect(allFeeds[i].url.length).toBeGreaterThan(0);
-						}
-				});
-
-				/* Loop through each feed in the allFeeds object and ensure
-				 * it has a name defined and that the name is not empty.
-				 */
-				 it('all should have a name', function() {
-						for (var i = 0; i < allFeeds.length; i++){
-							expect(allFeeds[i].name).toBeDefined();
-							expect(allFeeds[i].name.length).toBeGreaterThan(0);
-						}
-				});
+			/* Loop through each feed in the allFeeds object and ensure
+			 * it has a name defined and that the name is not empty.
+			 */
+			 it('all should have a name', function() {
+					for (var i = 0; i < allFeeds.length; i++){
+						expect(allFeeds[i].name).toBeDefined();
+						expect(allFeeds[i].name.length).toBeGreaterThan(0);
+					}
+			});
 		});
 
 
 		describe("The Menu", function() {
-				// Ensure the menu element is hidden by default.
-				it ('should be hidden by default', function() {
-					expect($('body.menu-hidden')).toBeDefined();
-				});
+			// Ensure the menu element is hidden by default.
+			it ('should be hidden by default', function() {
+				expect($('.menu-hidden').hasClass('menu-hidden')).toBe(true);
+			});
 
-				// Ensure the menu bar is shown when clicked.
-				it ('should shown when icon is clicked and toggled when icon is clicked again', function() {
-					$('a.menu-icon-link').trigger('click');
-					expect($('body').hasClass('menu-hidden')).toBeFalsy;
-					$('a.menu-icon-link').trigger('click');
-					expect($('body.menu-hidden')).toBeDefined();
-				});
+			// Ensure the menu bar is shown when clicked.
+			it ('should shown when icon is clicked and toggled when icon is clicked again', function() {
+				$('a.menu-icon-link').trigger('click');
+				expect($('body').hasClass('menu-hidden')).toBe(false);
+				$('a.menu-icon-link').trigger('click');
+				expect($('.menu-hidden').hasClass('menu-hidden')).toBe(true);
+			});
 		});
 
 		describe("Initial Entries", function() {
@@ -98,13 +97,9 @@ $(function() {
 				});
 			});
 
-			/* Here we have to make sure we call the success callback function done()
-			 *to inform jasmine that before each block is finished
-			 */
-			it('Ensure different feeds are loaded asynchrounously', function(done) {
+			it('Ensure different feeds are loaded asynchrounously', function() {
 				feedB = $('.feed').find("h2").text();
 				expect(feedA).not.toBe(feedB);
-				done();
 			});
 		});
 }());
